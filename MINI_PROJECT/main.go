1eaf9c7b6f4b6e3993e172447ae4e836caf8fe2b
Yohan Jirage <yjirage@gmail.com>
2024-03-20 21:12:34 +0530

    changes in Main

--- MINI_PROJECT/main.go
+++ MINI_PROJECT/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"log"
+
+	"github.com/YohanJirage/Go_Lang/config"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	
+}
+
 func main() {
 
+	err := godotenv.Load(".env")
+	errorCheck(err)
+
+	config.Db, err = config.DB_Connect()
+	if err != nil {
+		log.Fatal("Failed To Coonect")
+	}
+
+	r := Repository{
+		DB: config.Db,
+	}
+
+	app := fiber.New()
+	r.SetupRoutes(app)
+	app.Listen(":8080")
+
+	errorCheck(err)
+
+}
+
+func errorCheck(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
