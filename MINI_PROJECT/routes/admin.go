a75d63c1bdae23996e38039550deba5ea2ae989e
Yohan Jirage <yjirage@gmail.com>
2024-03-20 23:11:19 +0530

    Product functionality by Anup and category functionality by Yohan

--- MINI_PROJECT/routes/admin.go
+++ MINI_PROJECT/routes/admin.go
@@ -7,7 +7,12 @@ import (
 
 func AdminRoutes(app *fiber.App) {
 
-	user := app.Group("/admin")
-	user.Get("/GetCategories")
+	admin := app.Group("/admin")
+
+	admin.Post("/CreateCategory", controllers.AddCategory)
+	admin.Put("/UpdateCategory", controllers.EditCategory)
+	admin.Delete("/DeleteCategory", controllers.DeleteCategory)
+	admin.Get("/GetCategories", controllers.GetCategories)
+	admin.Post("/addProduct", controllers.AddProduct)
 
 }
